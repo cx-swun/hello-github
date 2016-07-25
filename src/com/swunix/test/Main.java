ef0b323b1d3616a442a9abdb63fce728f82c7034
cx-swun <chullsea@outlook.com>
2016-07-25 13:51:19 +0800

    commit from macbook pro

--- src/com/swunix/test/Main.java
+++ src/com/swunix/test/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello github! this is some newly added content. ");
+		System.out.println("edited from macbook pro");
 	}
 
 }
