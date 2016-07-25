4f73c2b371d349bd9935c893b7f61e20d84de312
cx-swun <chullsea@outlook.com>
2016-07-25 13:42:47 +0800

    second commit

--- src/com/swunix/test/Main.java
+++ src/com/swunix/test/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello github!");
+		System.out.println("hello github! this is some newly added content. ");
 	}
 
 }
