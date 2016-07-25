770b0232fd8ec5d21cf4297557834e3cbc1da1eb
cx-swun <chullsea@outlook.com>
2016-07-25 13:52:56 +0800

    4th commit form mac

---
+++ src/com/swunix/test/SecondMain.java
@@ -0,0 +1,10 @@
+package com.swunix.test;
+
+public class SecondMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("good boy in the second world. ");
+	}
+
+}
