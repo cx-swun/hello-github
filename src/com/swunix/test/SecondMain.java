a657304e0a5bf90699ef1ddc0f199c85eaca4514
cx-swun <chullsea@outlook.com>
2016-07-25 13:53:08 +0800

    5th commit

--- src/com/swunix/test/SecondMain.java
+++ src/com/swunix/test/SecondMain.java
@@ -5,6 +5,7 @@ public class SecondMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("good boy in the second world. ");
+		System.out.println("tree new bee");
 	}
 
 }
